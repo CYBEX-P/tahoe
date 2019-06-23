f7fc23b5a1ade06004d8c8f9805584c0f3aab7e3
qclass <qclass@tutanota.com>
2019-06-23 16:30:21 -0700

    Test

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tahoe",
+    version="0.0.1",
+    author="Farhan Sadique",
+    author_email="qclass@protonmail.com",
+    description="Cyber Threat Information Sharing Language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/tahoe",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
