f51f462c449f1f9e0bed02302b646183aea67523
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-07-16 10:21:53 -0700

    pretty print

--- DAM_demo.py
+++ DAM_demo.py
@@ -20,6 +20,7 @@ u3 = User("u3@abc.com")
 print("user1:",u1._hash)
 print("user2:",u2._hash)
 print("user3:",u3._hash)
+print()
 
 org1 = Org("org of user1",[u1, u2],[u1],"u1_Org")
 org2 = Org("org of user2",[u3, u2],[u2],"u2_Org")
@@ -40,17 +41,30 @@ d1 = [att1,att2]
 d2 = [att3,att4]
 d3 = [att5,att6]
 
+
 event_by_org1 = Event("sighting",d1, org1._hash, time.time())
 event_by_org2 = Event("sighting",d2, org2._hash, time.time())
 event_by_org_r = Event("sighting",d3, r, time.time())
 
+events = dict()
+events["event1_by_org1"] = [a.doc for a in [att1,att2]]
+events["event2_by_org2"] = [a.doc for a in [att3,att4]]
+events["event3_by_org_random"] = [a.doc for a in [att5,att6]]
+pprint(events)
+print()
+
 query = {'itype': 'event'}
 filter = {"_hash":1, "data":1,"itype":1,"_id":0 }
 
-r_control = b1.find(query )
+print("Query:",query)
+print("FIlter:",filter)
+print()
+
+r_control = b1.find(query,filter)
 r1 = DAM_backend.find(u1,query,filter)
 r2 = DAM_backend.find(u2,query,filter)
 r3 = DAM_backend.find(u3,query,filter)
+print()
 
 print("######### Control ################")
 pprint([t for t in r_control])
