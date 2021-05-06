5f07e816b266ca430243bcd3c9460c36f8b14af1
qclass <qclass@tutanota.com>
2021-05-06 12:47:02 -0700

    Identity

--- tahoe/identity/__init__.py
+++ tahoe/identity/__init__.py
@@ -2,4 +2,4 @@ from .backend import IdentityBackend, MockIdentityBackend
 from .identity import Identity
 from .org import Org
 from .user import User, SuperUser
-from .config import InputConfig
+from .config import InputConfig, WebSocketConfig
